f20e20f2f6d2445cab12d9017d9b6b3a3f2c3d96
Dan Wexler <wex@zorroa.com>
2016-09-29 04:23:13 -0700

    Initial commit

---
+++ src/components/posts_new.js
@@ -0,0 +1,81 @@
+import _ from 'lodash'
+import React, { Component, PropTypes } from 'react'
+import { reduxForm } from 'redux-form'
+import { createPost } from '../actions/index'
+import { Link } from 'react-router'
+
+const FIELDS = {
+  title: {
+    type: 'input',
+    label: 'Title'
+  },
+  categories: {
+    type: 'input',
+    label: 'Enter some categories for this post'
+  },
+  content: {
+    type: 'textarea',
+    label: 'Post contents'
+  }
+}
+
+class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  onSubmit (props) {
+    this.props.createPost(props)
+      .then(() => {
+        // blog post created, navigate to index
+        this.context.router.push('/')
+      })
+  }
+
+  renderField (fieldConfig, field) {
+    const fieldHelper = this.props.fields[field]
+    return (
+      <div className={`form-group ${fieldHelper.touched && fieldHelper.invalid ? 'has-danger' : ''}`}>
+        <label>{fieldConfig.label}</label>
+        <fieldConfig.type type="text" className="form-control" {...fieldHelper} />
+        <div className="text-help">
+          {fieldHelper.touched ? fieldHelper.error : ''}
+        </div>
+      </div>
+    )
+  }
+  render () {
+    const { handleSubmit } = this.props
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <h3>Create A New Post</h3>
+        {_.map(FIELDS, this.renderField.bind(this))}
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
+    )
+  }
+}
+
+function validate (values) {
+  const errors = {}
+  _.each(FIELDS, (type, field) => {
+    "use strict";
+    if (!values[field]) {
+      errors[field] = `Enter a ${field}`
+    }
+  })
+  return errors
+}
+
+PostsNew.propTypes = {
+  handleSubmit: React.PropTypes.func,
+  createPost: React.PropTypes.func,
+  fields: React.PropTypes.object
+}
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: _.keys(FIELDS),
+  validate
+}, null, { createPost })(PostsNew)
